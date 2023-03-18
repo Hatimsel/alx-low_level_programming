566c9c8c3470819ec80c3e6a6da0e186d764e88a
Hatimsel <selmun13@gmail.com>
2023-03-18 00:48:33 +0000

    if statement

--- 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x00-hello_world/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 /* betty style doc for function main goes there 
 * main - Entry point
@@ -11,13 +12,17 @@ int main(void)
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-				if n > 0
-				then
-					echo "$n is positive"
-				elseif n < 0
-				then
-					echo "$n is negative"
+				if (n > 0)
+				{
+					printf("%d is positive\n", n);
+				}
+				else if (n == 0)
+				{
+					printf("%d is zero\n", n);
+				}
 				else
-					echo "$n is zero"
+				{
+					printf("%d is negative\n", n);
+				}
 				return (0);
 }
