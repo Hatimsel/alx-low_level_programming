c475749a522b0cf77dd38e07ce614786ac4208de
Hatimsel <selmun13@gmail.com>
2023-03-23 17:47:59 +0000

    prints line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,18 +3,22 @@
 /**
   * print_line - prints a straight line
   * @n: the number of times the character _ should be printed
-  * @pnt: printing underscore
   */
 
 void print_line(int n)
 {
-	char pnt = 98 * n;
-
-	if (n == 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
-		_putchar(pnt);
-	_putchar('\n');
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
