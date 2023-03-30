510979bbbc58d021e81f5b91c99dfbec9fda730b
Hatimsel <selmun13@gmail.com>
2023-03-30 00:27:04 +0000

    concatenation of two strings at most n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  _strncat - Concatenates two strings.
+ *
+ *  @dest: The destination string.
+ *
+ *  @src: The source string.
+ *
+ *  @n: The number of bytes from src to be added to dest.
+ *
+ *  Return: A pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+	int i = 0, length = 0;
+
+	while (dest[i++])
+	length++;
+	for (i = 0; src[i] && i < n; i++)
+	dest[length++] = src[i];
+	return (dest)
+}
