432c3de0b0d0d35d9a56340d076e82f063a33700
Hatimsel <selmun13@gmail.com>
2023-03-31 01:25:33 +0000

    capitalize on

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
+ * cap_string - Capitalizes letters
  *
+ *@str: string to be capitalized
  *
- *
- *
- *
+ *Return: returns str capitalized
  */
 
 char *cap_string(char *str)
@@ -27,21 +27,23 @@ char *cap_string(char *str)
 		}
 		if (str[i] == 9 || str[i] == 10 || ((str[i] == 46) && (str[i + 1] != 32)))
 			str[i + 1] -= 32;
-		/*if (str[i] == 9)
-		{
-			str[i + 1] -= 32;
-		}
-		if (str[i] == 46)
-		{
-			if (str[i + 1] != 32)
-			{
-				str[i + 1] -= 32;
-			}
-		}
-		if (str[i] == 10)
-		{
-			str[i] -= 32;
-		}*/
+		/**
+		* if (str[i] == 9)
+		*{
+		*	str[i + 1] -= 32;
+		*}
+		*if (str[i] == 46)
+		*{
+		*	if (str[i + 1] != 32)
+		*	{
+		*		str[i + 1] -= 32;
+		*	}
+		*}
+		*if (str[i] == 10)
+		*{
+		*	str[i] -= 32;
+		*}
+		*/
 		i++;
 	}
 	return (str);
