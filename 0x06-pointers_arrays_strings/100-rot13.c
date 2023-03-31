832f952efa7005a4c822201fc676bb0263fead85
Hatimsel <selmun13@gmail.com>
2023-03-31 23:51:36 +0000

    rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ *
+ * @s: the string to be encoded
+ *
+ * Return: return the encoded string
+ *
+ */
+
+char *rot13(char *s)
+{
+	int i  = 0;
+
+	while (s[i])
+	{
+
+	while ((s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z'))
+	{
+		if (s[i] >= 'a' && s[i] <= 'm' || s[i] >= 'A' && s[i] <= 'M')
+		{
+			s[i] += 13;
+			break;
+		}
+
+		s[i] -= 13;
+		break;
+	}
+
+	i++;
+	}	}
+	return (s);
+}
