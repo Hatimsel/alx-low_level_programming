9e97b46d4aaab4b8e3701965457a6d889a885e6c
Hatimsel <selmun13@gmail.com>
2023-03-31 02:50:47 +0000

    capitalize on

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,37 +14,11 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		if (str[i] == 32)
-		{
-			if (str[i + 1] >= 65 && str[i + 1] <= 90)
-			{
-				str[i + 1] = str[i + 1];
-			}
-			else
-			{
-				str[i + 1] -= 32;
-			}
-		}
-		if (str[i] == 9 || str[i] == 10 || ((str[i] == 46) && (str[i + 1] != 32)))
-			str[i + 1] -= 32;
-		/**
-		* if (str[i] == 9)
-		*{
-		*	str[i + 1] -= 32;
-		*}
-		*if (str[i] == 46)
-		*{
-		*	if (str[i + 1] != 32)
-		*	{
-		*		str[i + 1] -= 32;
-		*	}
-		*}
-		*if (str[i] == 10)
-		*{
-		*	str[i] -= 32;
-		*}
-		*/
-		i++;
+		while(!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+	if (str[i -1] == ' '|| str [i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
+		str[i] -= 32;
+	i++;
 	}
 	return (str);
 }
