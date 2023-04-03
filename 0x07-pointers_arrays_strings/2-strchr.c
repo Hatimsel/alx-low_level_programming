2dab86c2bb2fba8b96796d4cbc4fdfdd3836124e
Hatimsel <selmun13@gmail.com>
2023-04-03 16:40:34 +0000

    locating a chr in astring

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry Point
+ *
+ * @s: string to look in
+ *
+ * @c: character to look for
+ *
+ * Return: returns s+i or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
