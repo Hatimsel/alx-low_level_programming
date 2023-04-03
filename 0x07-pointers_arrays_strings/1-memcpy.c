a9bcfa969b0771ea06aefb9ef01c8a7b72be660c
Hatimsel <selmun13@gmail.com>
2023-04-03 15:37:43 +0000

    memory copu

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,14 @@
 
 /**
  * _memcpy - Entry point
- * @dest: input
- * @src: input
- * @n: input
- * Return: Always 0 (Success)
+ *
+ * @dest: destination
+ *
+ * @src: source memory address
+ *
+ * @n: number of bytes
+ *
+ * Return: Returns destination
  */
 
 
@@ -15,7 +19,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		src[i] = dest[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
