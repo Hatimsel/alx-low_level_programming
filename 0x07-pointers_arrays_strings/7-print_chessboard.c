b64e5148b506ff0522aaf431b42274a585b7bc61
Hatimsel <selmun13@gmail.com>
2023-04-04 01:37:35 +0000

    print a chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,10 +10,10 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-	for (j = 0; j < 8; j++)
-	{
-		printf("%c ", board[i][j]);
-	}
-		printf("\n");
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
 }
