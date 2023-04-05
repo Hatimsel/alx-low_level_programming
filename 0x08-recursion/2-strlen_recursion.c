49afeb405468178c304bab101c9134ae2348ba90
Hatimsel <selmun13@gmail.com>
2023-04-05 23:19:45 +0000

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Entry point
+ *
+ * @s: string to be measured
+ *
+ * Return: return an integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
