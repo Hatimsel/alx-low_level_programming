60a054d13355d213d1e0feeb8f374a16410b1af4
Hatimsel <selmun13@gmail.com>
2023-04-11 22:38:29 +0000

    concatenating

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Entry point
+ *
+ * @s1: string num 1
+ * @s2: string number two
+ *
+ * Return: return a pointer to the concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1;
+	int len2;
+	char *concat;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(concat, s1);
+	strcat(concat, s2);
+
+	return (concat);
+	free(concat);
+}
