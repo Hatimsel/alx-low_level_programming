bde057fd8549c44e7e68c54f36568b47fc8106cc
Hatimsel <selmun13@gmail.com>
2023-04-19 15:09:55 +0000

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - iterates through an array
+*
+* @array: the array provided
+* @size: the size of the array
+* @action: pointer to a function
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
