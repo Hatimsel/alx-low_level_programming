1869d86c8167eb3e8d32b44391a7ca62502041db
Hatimsel <selmun13@gmail.com>
2023-04-20 00:35:40 +0000

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,36 +12,30 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int a = 0;
-	void (*fptr)(void) = (void (*)(void)) &main;
-	unsigned char opcode;
+	char *opc = (char *) main;
+	int i, nbytes;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	a = atoi(argv[1]);
 
-	if (a < 0)
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	for (i = 0; i < a; i++)
+	for (i = 0; i < nbytes; i++)
 	{
-		opcode = *(unsigned char *)fptr;
-
-		printf("%02x ", opcode);
-
-		if (i != a - 1)
-		{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
 			printf(" ");
-		}
-		fptr++;
 	}
+
 	printf("\n");
 	return (0);
 }
