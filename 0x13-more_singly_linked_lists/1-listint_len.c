965666d07b9512ac118c87a9c59a96b52da2fe6f
Hatimsel <selmun13@gmail.com>
2023-04-29 07:05:58 +0000

    num of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* listint_len - prints the length of a linked list
+*
+* @h: a pointer to listint_t
+*
+* Return: returns the number of nodes
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+	return (1);
+	}
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
