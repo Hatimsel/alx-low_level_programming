428d78c8501381c665404cb25cd4755593b91d62
Hatimsel <selmun13@gmail.com>
2023-04-29 16:39:27 +0000

    sum of n

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* sum_listint - calculates the sum of n
+*
+* @head: a pointer to the first node
+* Return: returns the sum
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+	return (0);
+	}
+	while (head != NULL)
+	{
+	sum += head->n;
+	head = head->next;
+	}
+	return (sum);
+}
