be4bb6c1813f3221be6ad7d6959eae067730b4b2
Hatimsel <selmun13@gmail.com>
2023-04-29 15:00:31 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees memory
+*
+* @head: doule pointer
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr = *head;
+	listint_t *aux;
+
+	while (curr != NULL)
+	{
+		aux = curr;
+		curr = curr->next;
+		free(aux);
+	}
+}
