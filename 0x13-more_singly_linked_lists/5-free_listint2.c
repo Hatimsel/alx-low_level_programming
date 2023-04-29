8a6e323381e9798dd34c475b2676d4905a28e27f
Hatimsel <selmun13@gmail.com>
2023-04-29 15:07:09 +0000

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,8 +8,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *curr = *head;
+	listint_t *curr;
 	listint_t *aux;
+
+	if (head == NULL)
+	{
+	return;
+	}
+	curr = *head;
 
 	while (curr != NULL)
 	{
