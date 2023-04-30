7a528610d26fcfad19f178c05c78cc5b78784198
Hatimsel <selmun13@gmail.com>
2023-04-30 17:20:53 +0100

    prints elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,9 +14,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-	printf("%d\n", h->n);
-	h = h->next;
-	count++;
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
+
 	return (count);
 }
