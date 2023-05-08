3ecd75563645eeb9509da79e11f7443d46d5e882
Hatimsel <selmun13@gmail.com>
2023-05-08 20:02:37 +0100

    error handling

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
-char _putchar(void);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
