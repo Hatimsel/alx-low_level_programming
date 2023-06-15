f3d3654c835956b3808e2d481f8c7ae771ff61c0
Hatimsel <selmun13@gmail.com>
2023-06-15 17:28:20 +0100

    list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* slistint_len - list length
+*
+* @h: a pointer to the head of  alinked list
+* Return: returns the number of elements
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
