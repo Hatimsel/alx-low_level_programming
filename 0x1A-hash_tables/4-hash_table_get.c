3bfa53eb528fbceeecb0f89f76f710ca783b538f
Hatimsel <selmun13@gmail.com>
2023-07-07 19:32:20 +0100

    fixing hash table get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *curr;
-	unsigned long int i = 0;
+	int i = 0;
 
 	i = key_index((const unsigned char *)key, ht->size);
 	curr = ht->array[i];
