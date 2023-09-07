aa3e101a0981a263af334acbccbc61836d417b7c
Hatimsel <selmun13@gmail.com>
2023-09-07 19:36:06 +0100

    Inserting a new node

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Inserts a new node
+ *
+ * @ht: a pointer to our hash table
+ * @key: the key element
+ * @value: the value element
+ *
+ * Return: returns 1 if succeeded else 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	const unsigned char *un_key = (const unsigned char *)key;
+	unsigned long int index = key_index(un_key, ht->size);
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *head = ht->array[index];
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	if (head == NULL)
+	{
+		head = new_node;
+	}
+
+	while (head)
+		head = head->next;
+
+	head = new_node;
+
+	return (1);
+}
