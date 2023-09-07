ddd0c4e655095e20a0c2b88fb414ec0ba4d1ec82
Hatimsel <selmun13@gmail.com>
2023-09-07 19:59:59 +0100

    Inserting a new node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,8 +38,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		tmp = ht->array[index];
+		if (strcmp(key, tmp->value) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		else
+		{
 		new_node->next = tmp;
 		ht->array[index] = new_node;
+		}
 	}
 
 	return (1);
