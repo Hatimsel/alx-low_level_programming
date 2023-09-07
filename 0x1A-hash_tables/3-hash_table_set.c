c053ea48d3cf1da22a175e89cf9e020e34f35b12
Hatimsel <selmun13@gmail.com>
2023-09-07 19:55:24 +0100

    Inserting a new node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	if (strlen(value) > 0 && value != NULL)
+		new_node->value = strdup(value);
+	else
+		new_node->value = strdup("");
 	new_node->next = NULL;
 
 	if (ht->array[index] == NULL)
