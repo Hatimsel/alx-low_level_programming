15d25084c93e52f5b46e18b2278ea474c5b0f58c
Hatim Selmun <selmun13@gmail.com>
2024-02-06 11:10:19 +0100

    fixing linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+
 /**
 * linear_search - Searches for a value in an array of integers
 *
@@ -18,7 +19,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = %d\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
